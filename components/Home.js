c8fd72b199b38c95378c1da441b8d83bb7a9f15d
R4z1ell <creawshaka@gmail.com>
2019-03-14 19:03:02 +0100

    started working on the Home Component

--- components/Home.js
+++ components/Home.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import start from '../utils/starfield';
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Inconsolata:700|Ubuntu:400,700|Varela+Round');
+
   html {
     box-sizing: border-box;
     font-size: 10px;
@@ -21,6 +23,7 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     overflow-x: hidden;
     font-size: 1.6rem;
+    font-family: 'Varela Round';
     line-height: 2;
   }
 
@@ -29,17 +32,76 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+const StyledCanvas = styled.canvas`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100%;
+`;
+
+const Flex = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Text = styled.div`
+  font-size: 4.6rem;
+  line-height: 5.4rem;
+  text-align: center;
+  pointer-events: none;
+  margin-bottom: 15px;
+  color: #fff;
+  z-index: 1;
+
+  span {
+    color: #fc3565;
+    margin-left: 10px;
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const Button = styled.button`
+  background: ${props => (props.primary ? '#fc3565' : 'transparent')};
+  padding: 18px 40px;
+  margin-right: 20px;
+  font-family: inherit;
+  font-size: 18px;
+  color: #fff;
+  cursor: pointer;
+  border: ${props => (props.primary ? 'none' : '1px solid #fff')};
+  z-index: 1;
+`;
+
 const Home = () => {
   useEffect(() => {
     setTimeout(() => {
       start();
-    }, 200);
+    }, 0);
   }, []);
 
   return (
     <React.Fragment>
       <GlobalStyle />
-      <canvas id="canvas" width="100vw" height="100vh" />
+      <StyledCanvas id="canvas" />
+      <Flex>
+        <Text>
+          Hello, I'm
+          <span>Pietro</span>.
+          <br />
+          I'm a Full-stack Web Developer from Italy.
+        </Text>
+        <Wrapper>
+          <Button primary>My Projects</Button>
+          <Button>Contact me</Button>
+        </Wrapper>
+      </Flex>
     </React.Fragment>
   );
 };
